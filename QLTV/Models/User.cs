8068bc777d30a84c79e7c97e000af063494b05f9
ntdat27 <datnn288@gmail.com>
2026-04-20 21:07:59 +0700

    Hoàn thành chức năng Thống kê, vẽ biểu đồ Dashboard (Đạt)

--- QLTV/Models/User.cs
+++ QLTV/Models/User.cs
@@ -4,9 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace QLTV.Entities
+namespace QLTV.Models
 {
-    internal class User
+    public class User
     {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Status { get; set; }
+        public int RoleID { get; set; }
+
+        public string RoleName { get; set; }
     }
 }

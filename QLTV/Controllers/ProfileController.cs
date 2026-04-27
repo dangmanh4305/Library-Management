c3897033c5a450a4457d97503c4c6318c1de8714
Tài <tai20052603@gmail.com>
2026-04-27 21:23:34 +0700

    Add profile + book feature

---
+++ QLTV/Controllers/ProfileController.cs
@@ -0,0 +1,78 @@
+﻿using System.Data;
+using MySqlConnector;
+using QLTV.DBConnections;
+
+namespace QLTV.Controllers
+{
+    public class ProfileController
+    {
+        // Lấy thông tin user
+        public DataTable GetProfile(int userId)
+        {
+            string query = @"
+        SELECT 
+            u.Username,
+            u.FullName,
+            u.Status,
+            r.RoleName
+        FROM users u
+        LEFT JOIN roles r ON u.RoleID = r.RoleID
+        WHERE u.UserID = @id
+    ";
+
+            MySqlParameter[] parameters =
+            {
+        new MySqlParameter("@id", userId)
+    };
+
+            return DBConnection.GetDataTable(query, parameters);
+        }
+
+        // Cập nhật profile
+        public bool UpdateProfile(int userId, string name)
+        {
+            string query = @"UPDATE Users 
+                     SET FullName=@name
+                     WHERE UserID=@id";
+
+            MySqlParameter[] parameters =
+            {
+        new MySqlParameter("@name", name),
+        new MySqlParameter("@id", userId)
+    };
+
+            return DBConnection.ExecuteNonQuery(query, parameters) > 0;
+        }
+
+        // Đổi mật khẩu
+        public bool ChangePassword(int userId, string oldPass, string newPass)
+        {
+            string checkQuery = @"SELECT COUNT(*) FROM Users 
+                          WHERE UserID=@id AND PasswordHash=@old";
+
+            MySqlParameter[] checkParams =
+            {
+        new MySqlParameter("@id", userId),
+        new MySqlParameter("@old", oldPass)
+    };
+
+            int count = System.Convert.ToInt32(
+                DBConnection.ExecuteScalar(checkQuery, checkParams)
+            );
+
+            if (count == 0) return false;
+
+            string updateQuery = @"UPDATE Users 
+                           SET PasswordHash=@new 
+                           WHERE UserID=@id";
+
+            MySqlParameter[] updateParams =
+            {
+        new MySqlParameter("@new", newPass),
+        new MySqlParameter("@id", userId)
+    };
+
+            return DBConnection.ExecuteNonQuery(updateQuery, updateParams) > 0;
+        }
+    }
+}
--- QLTV/Views/WarehouseManager/frmBookManager.Designer.cs
+++ QLTV/Views/WarehouseManager/frmBookManager.Designer.cs
@@ -7,7 +7,7 @@
         private System.Windows.Forms.DataGridView dgvBooks;
         private System.Windows.Forms.TextBox txtTitle;
         private System.Windows.Forms.TextBox txtISBN;
-        private System.Windows.Forms.TextBox txtCategoryId;
+        private System.Windows.Forms.ComboBox cbCategory;
         private System.Windows.Forms.TextBox txtQuantity;
         private System.Windows.Forms.CheckBox chkIsRare;
         private System.Windows.Forms.Button btnAdd;
@@ -31,7 +31,7 @@
             this.dgvBooks = new System.Windows.Forms.DataGridView();
             this.txtTitle = new System.Windows.Forms.TextBox();
             this.txtISBN = new System.Windows.Forms.TextBox();
-            this.txtCategoryId = new System.Windows.Forms.TextBox();
+            this.cbCategory = new System.Windows.Forms.ComboBox();
             this.txtQuantity = new System.Windows.Forms.TextBox();
             this.chkIsRare = new System.Windows.Forms.CheckBox();
             this.btnAdd = new System.Windows.Forms.Button();
@@ -45,177 +45,241 @@
             this.txtSearch = new System.Windows.Forms.TextBox();
             this.qly = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvBooks
             // 
-            this.dgvBooks.Location = new System.Drawing.Point(103, 110);
+            this.dgvBooks.Location = new System.Drawing.Point(292, 142);
             this.dgvBooks.MultiSelect = false;
             this.dgvBooks.Name = "dgvBooks";
             this.dgvBooks.ReadOnly = true;
             this.dgvBooks.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvBooks.Size = new System.Drawing.Size(760, 200);
+            this.dgvBooks.Size = new System.Drawing.Size(1011, 345);
             this.dgvBooks.TabIndex = 2;
             this.dgvBooks.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvBooks_CellClick);
             // 
             // txtTitle
             // 
-            this.txtTitle.Location = new System.Drawing.Point(179, 352);
+            this.txtTitle.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitle.Location = new System.Drawing.Point(395, 511);
+            this.txtTitle.Multiline = true;
             this.txtTitle.Name = "txtTitle";
-            this.txtTitle.Size = new System.Drawing.Size(228, 20);
+            this.txtTitle.Size = new System.Drawing.Size(411, 31);
             this.txtTitle.TabIndex = 3;
             // 
             // txtISBN
             // 
-            this.txtISBN.Location = new System.Drawing.Point(179, 382);
+            this.txtISBN.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtISBN.Location = new System.Drawing.Point(395, 556);
+            this.txtISBN.Multiline = true;
             this.txtISBN.Name = "txtISBN";
-            this.txtISBN.Size = new System.Drawing.Size(228, 20);
+            this.txtISBN.Size = new System.Drawing.Size(411, 31);
             this.txtISBN.TabIndex = 4;
             // 
-            // txtCategoryId
+            // cbCategory
             // 
-            this.txtCategoryId.Location = new System.Drawing.Point(179, 412);
-            this.txtCategoryId.Name = "txtCategoryId";
-            this.txtCategoryId.Size = new System.Drawing.Size(228, 20);
-            this.txtCategoryId.TabIndex = 5;
+            this.cbCategory.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbCategory.Location = new System.Drawing.Point(395, 598);
+            this.cbCategory.Name = "cbCategory";
+            this.cbCategory.Size = new System.Drawing.Size(411, 31);
+            this.cbCategory.TabIndex = 5;
             // 
             // txtQuantity
             // 
-            this.txtQuantity.Location = new System.Drawing.Point(179, 442);
+            this.txtQuantity.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuantity.Location = new System.Drawing.Point(395, 641);
+            this.txtQuantity.Multiline = true;
             this.txtQuantity.Name = "txtQuantity";
-            this.txtQuantity.Size = new System.Drawing.Size(228, 20);
+            this.txtQuantity.Size = new System.Drawing.Size(411, 31);
             this.txtQuantity.TabIndex = 6;
             // 
             // chkIsRare
             // 
-            this.chkIsRare.Location = new System.Drawing.Point(179, 472);
+            this.chkIsRare.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkIsRare.Location = new System.Drawing.Point(395, 686);
             this.chkIsRare.Name = "chkIsRare";
-            this.chkIsRare.Size = new System.Drawing.Size(104, 24);
+            this.chkIsRare.Size = new System.Drawing.Size(208, 24);
             this.chkIsRare.TabIndex = 7;
             this.chkIsRare.Text = "Sách hiếm";
             // 
             // btnAdd
             // 
-            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdd.Location = new System.Drawing.Point(429, 352);
+            this.btnAdd.BackColor = System.Drawing.Color.Lime;
+            this.btnAdd.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.Black;
+            this.btnAdd.Location = new System.Drawing.Point(21, 115);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(75, 30);
+            this.btnAdd.Size = new System.Drawing.Size(154, 49);
             this.btnAdd.TabIndex = 8;
             this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = false;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(429, 432);
+            this.btnDelete.BackColor = System.Drawing.Color.Red;
+            this.btnDelete.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnDelete.Location = new System.Drawing.Point(21, 292);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.Size = new System.Drawing.Size(154, 51);
             this.btnDelete.TabIndex = 10;
             this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = false;
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnRefresh
             // 
-            this.btnRefresh.Location = new System.Drawing.Point(429, 472);
+            this.btnRefresh.BackColor = System.Drawing.Color.Yellow;
+            this.btnRefresh.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefresh.Location = new System.Drawing.Point(21, 380);
             this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.Size = new System.Drawing.Size(154, 55);
             this.btnRefresh.TabIndex = 11;
             this.btnRefresh.Text = "Làm mới";
+            this.btnRefresh.UseVisualStyleBackColor = false;
             this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(429, 392);
+            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnUpdate.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.ForeColor = System.Drawing.Color.Black;
+            this.btnUpdate.Location = new System.Drawing.Point(21, 202);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.Size = new System.Drawing.Size(154, 53);
             this.btnUpdate.TabIndex = 9;
             this.btnUpdate.Text = "Sửa";
+            this.btnUpdate.UseVisualStyleBackColor = false;
             this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // lblTitle
             // 
-            this.lblTitle.Location = new System.Drawing.Point(91, 352);
+            this.lblTitle.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(291, 515);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(100, 23);
+            this.lblTitle.Size = new System.Drawing.Size(180, 23);
             this.lblTitle.TabIndex = 12;
             this.lblTitle.Text = "Tên sách";
             // 
             // lblISBN
             // 
-            this.lblISBN.Location = new System.Drawing.Point(91, 382);
+            this.lblISBN.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblISBN.Location = new System.Drawing.Point(291, 559);
             this.lblISBN.Name = "lblISBN";
-            this.lblISBN.Size = new System.Drawing.Size(100, 23);
+            this.lblISBN.Size = new System.Drawing.Size(180, 23);
             this.lblISBN.TabIndex = 13;
             this.lblISBN.Text = "ISBN";
             // 
             // lblCategory
             // 
-            this.lblCategory.Location = new System.Drawing.Point(91, 412);
+            this.lblCategory.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategory.Location = new System.Drawing.Point(291, 605);
             this.lblCategory.Name = "lblCategory";
-            this.lblCategory.Size = new System.Drawing.Size(100, 23);
+            this.lblCategory.Size = new System.Drawing.Size(180, 23);
             this.lblCategory.TabIndex = 14;
             this.lblCategory.Text = "Thể loại";
             // 
             // lblQuantity
             // 
-            this.lblQuantity.Location = new System.Drawing.Point(91, 442);
+            this.lblQuantity.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuantity.Location = new System.Drawing.Point(291, 644);
             this.lblQuantity.Name = "lblQuantity";
-            this.lblQuantity.Size = new System.Drawing.Size(100, 23);
+            this.lblQuantity.Size = new System.Drawing.Size(180, 23);
             this.lblQuantity.TabIndex = 15;
             this.lblQuantity.Text = "Số lượng";
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(663, 326);
+            this.txtSearch.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(939, 512);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(200, 20);
+            this.txtSearch.Size = new System.Drawing.Size(364, 32);
             this.txtSearch.TabIndex = 0;
             this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
             // 
             // qly
             // 
             this.qly.AutoSize = true;
-            this.qly.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.qly.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.qly.Location = new System.Drawing.Point(87, 35);
+            this.qly.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.qly.ForeColor = System.Drawing.Color.White;
+            this.qly.Location = new System.Drawing.Point(98, 32);
             this.qly.Name = "qly";
-            this.qly.Size = new System.Drawing.Size(308, 42);
+            this.qly.Size = new System.Drawing.Size(302, 42);
             this.qly.TabIndex = 16;
             this.qly.Text = "QUẢN LÝ SÁCH";
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(593, 329);
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(831, 515);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 23);
+            this.label1.Size = new System.Drawing.Size(102, 23);
             this.label1.TabIndex = 17;
             this.label1.Text = "Tìm kiếm";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.btnRefresh);
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.btnUpdate);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(194, 882);
+            this.panel1.TabIndex = 18;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(192, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1199, 90);
+            this.panel2.TabIndex = 12;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel3.Controls.Add(this.qly);
+            this.panel3.Location = new System.Drawing.Point(194, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1206, 112);
+            this.panel3.TabIndex = 19;
+            // 
             // frmBookManager
             // 
-            this.ClientSize = new System.Drawing.Size(954, 565);
+            this.ClientSize = new System.Drawing.Size(1404, 753);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.qly);
             this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.dgvBooks);
             this.Controls.Add(this.txtTitle);
             this.Controls.Add(this.txtISBN);
-            this.Controls.Add(this.txtCategoryId);
+            this.Controls.Add(this.cbCategory);
             this.Controls.Add(this.txtQuantity);
             this.Controls.Add(this.chkIsRare);
-            this.Controls.Add(this.btnAdd);
-            this.Controls.Add(this.btnUpdate);
-            this.Controls.Add(this.btnDelete);
-            this.Controls.Add(this.btnRefresh);
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.lblISBN);
             this.Controls.Add(this.lblCategory);
             this.Controls.Add(this.lblQuantity);
             this.Name = "frmBookManager";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Quản lý sách";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.Load += new System.EventHandler(this.frmBookManager_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -223,5 +287,8 @@
 
         private System.Windows.Forms.Label qly;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
     }
 }
